1a641a30270eb51325e91ff0b694be087cc33584
Netlight <jasoness@yandex.ru>
2018-08-26 16:30:30 +0300

    Add handlers and main menu
    * Add Menu, Start, NotImplemented, RunLastTimer, Timers, WrongInput
    handlers
    * Add "Таймеры", "Запустить последний таймер", "Вернуться в меню"
    buttons

--- app/entry_points.py
+++ app/entry_points.py
@@ -1,8 +1,11 @@
-from telegram import ext
-
 import app.handlers
 
 
 handlers = [
-    ext.RegexHandler(r'/start', app.handlers.start),
+    app.handlers.Start(r'/start'),
+    app.handlers.Menu(app.handlers.Menu.RETURN_TO_MENU_BTN),
+    app.handlers.RunLastTimer(app.handlers.Start.START_LAST_TIMER_BTN),
+    app.handlers.Timers(app.handlers.Start.TIMERS_BTN),
+    # All handlers below following will be ignored.
+    app.handlers.WrongInput(r'.*'),
 ]
--- app/handlers.py
+++ app/handlers.py
@@ -1,11 +1,59 @@
 import mongoengine.errors
 
 from schemas.users import User
+import app.interfaces
 
 
-def start(bot, update):
-    user_id = update.effective_message.from_user.id
-    try:
-        User.objects.create(telegram_id=user_id)
-    except mongoengine.errors.NotUniqueError:
+class NotImplemented(app.interfaces.RegexHandler):
+    reply_text = 'Ого! Кажется эта функция еще в разработке!'
+
+    def callback(self, bot, update):
+        pass
+
+    def get_markup_keyboard(self):
+        return [
+            [Menu.RETURN_TO_MENU_BTN],
+        ]
+
+
+class Menu(app.interfaces.RegexHandler):
+    TIMERS_BTN = 'Таймеры'
+    START_LAST_TIMER_BTN = 'Запустить последний таймер'
+    RETURN_TO_MENU_BTN = 'Вернуться в меню'
+
+    reply_text = 'Выберите действие'
+
+    def callback(self, bot, update):
+        pass
+
+    def get_markup_keyboard(self):
+        return [
+            [self.TIMERS_BTN],
+            [self.START_LAST_TIMER_BTN],
+        ]
+
+
+class Start(Menu):
+    def callback(self, bot, update):
+        try:
+            User.objects.create(telegram_id=self.user.id)
+        except mongoengine.errors.NotUniqueError:
+            pass
+
+    def get_reply_text(self):
+        return f'Привет, {self.user.username}!\nДавай сразу приступим.'
+
+
+class RunLastTimer(NotImplemented):
+    pass
+
+
+class Timers(NotImplemented):
+    pass
+
+
+class WrongInput(app.interfaces.RegexHandler):
+    reply_text = 'Я ничего не понимаю.'
+
+    def callback(self, bot, update):
         pass
