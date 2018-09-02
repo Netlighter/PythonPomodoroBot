10970c65312eaf9861a253bfdb10c1c94e1dede3
Nikita <anon731@protonmail.com>
2018-09-03 00:41:19 +0300

    Change users.schemas indentation.

--- users/schemas.py
+++ users/schemas.py
@@ -2,6 +2,5 @@ import mongoengine
 
 
 class User(mongoengine.Document):
-    telegram_id = mongoengine.IntField(
-        primary_key=True,
-        required=True)
+    telegram_id = mongoengine.IntField(primary_key=True, required=True)
+    state = mongoengine.StringField(null=True)
